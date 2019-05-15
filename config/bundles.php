687a266b2248e0b0aff1101e7b631238b6c2ae2a
kubikkk <ljustpewpewl@gmail.com>
2019-05-15 11:34:14 +0300

    Add cs-fix files

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,10 @@
 <?php
 
+/*
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
